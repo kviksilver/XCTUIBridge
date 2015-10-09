bca73f783ea090596b745ac580d1290197ed9903
kviksilver <berceg@gmail.com>
2015-10-08 22:37:04 -0400

    make simple first

--- Example/XCTUIBridge/AppDelegate.swift
+++ Example/XCTUIBridge/AppDelegate.swift
@@ -10,15 +10,15 @@ import UIKit
 import XCTUIBridge
 
 @UIApplicationMain
-class AppDelegate: UIResponder, UIApplicationDelegate, XCTUIBridgeDelegate {
+class AppDelegate: UIResponder, UIApplicationDelegate {
 
     var window: UIWindow?
-    var bridge: XCTUIBridge?
+
 
 
     func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
         // Override point for customization after application launch.
-        bridge = XCTUIBridge(delegate: self)
+     
         return true
     }
 
@@ -42,13 +42,6 @@ class AppDelegate: UIResponder, UIApplicationDelegate, XCTUIBridgeDelegate {
 
     func applicationWillTerminate(application: UIApplication) {
         // Called when the application is about to terminate. Save data if appropriate. See also applicationDidEnterBackground:.
-    }
-
-    
-    //MARK: XCTUIBridgeDelegate
-    
-    func bridgeReceivedMessage(message: XCTUIBridgeMessage) -> XCTUIBridgePayload? {
-        return nil
     }
 
 }
--- Pod/Classes/XCTUIBridge.swift
+++ Pod/Classes/XCTUIBridge.swift
@@ -8,148 +8,54 @@
 
 import Foundation
 
-public typealias XCTUIBridgePayload = [String: AnyObject]
-public typealias XCTUIBridgeCompletion = (XCTUIBridgePayload?)->Void
-
-enum XCTUIBridgeError: ErrorType {
-    case MissingSelector
-}
-
-public protocol XCTUIBridgeDelegate: class {
-    func bridgeReceivedMessage(message: XCTUIBridgeMessage) -> XCTUIBridgePayload?
-}
-
-public struct XCTUIBridgeMessage {
-    
-    let selector: Selector
-    let identifier: String
-    let data: XCTUIBridgePayload?
-    
-    public init(selector: Selector, data:XCTUIBridgePayload? = nil) {
-        self.selector = selector
-        self.identifier = String(arc4random())
-        self.data = data
-    }
-    
-    init(dictionary: [String:AnyObject]) throws {
-        guard let selectorFromDict = dictionary["selector"] as? String,
-            identifierFromDict = dictionary["id"] as? String else {
-                
-            throw XCTUIBridgeError.MissingSelector
-        }
-        
-        selector = Selector(selectorFromDict)
-        if let dataFromDict = dictionary["data"] as? XCTUIBridgePayload {
-            data = dataFromDict
-        } else {
-            data = nil
-        }
-        identifier = identifierFromDict
-        
-    }
-    
-    func serialized() -> [String:AnyObject] {
-        var dictionary = [String:AnyObject]()
-        if let data = data {
-            dictionary["data"] = data
-        }
-        dictionary["id"] = identifier
-        //TODO: better way to get string selector from string?
-        dictionary["selector"] = selector.description
-    
-        
-        return dictionary
-    }
-}
-
-enum XCTUIBridgeSide: String {
-    case Test = "XCTUIBridgeSideTest"
-    case Client = "XCTUIBridgeSideClient"
-}
+public typealias XCTUIBridgeRemover = () -> Void;
+public typealias XCTUIBridgeCallback = () -> Void;
 
 let XCTUIBridgeNotification = "XCTUIBridgeNotification"
+let instance = XCTUIBridge()
 
-public final class XCTUIBridge: NSObject {
-    private weak var delegate: XCTUIBridgeDelegate?
-    var callbacks = [String: XCTUIBridgeCompletion]()
+public class XCTUIBridge {
+    private var clientListeners = [String: [XCTUIBridgeCallback]]();
     
-    public init(delegate: XCTUIBridgeDelegate) {
-        self.delegate = delegate
-        super.init()
-        registerClientSide()
-    }
-    
-    public override init() {
-        super.init()
-    }
-    public func sendMessage(message: XCTUIBridgeMessage, completion: XCTUIBridgeCompletion) {
-        registerTestSide()
-        callbacks[message.identifier] = completion
-        XCTUIBridge.sendDarwinNotificationWithIdentifier(XCTUIBridgeSide.Test.rawValue, payload: message.serialized())
-    }
-    
-    func registerClientSide() {
+    private init() {
         NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("notificationRecieved:"), name: XCTUIBridgeNotification, object: nil)
-        XCTUIBridge.registerForDarwinNotificationsWithIdentifier(XCTUIBridgeSide.Client.rawValue)
     }
-    
-    func registerTestSide() {
-        NSNotificationCenter.defaultCenter().addObserver(self, selector: Selector("notificationRecieved:"), name: XCTUIBridgeNotification, object: nil)
-        XCTUIBridge.registerForDarwinNotificationsWithIdentifier(XCTUIBridgeSide.Test.rawValue)
-    }
-    
-    func testSideBridgeRecivedMessage(message: XCTUIBridgeMessage) {
-        if let callback = callbacks[message.identifier] {
-            callback(message.data)
-        }
-        //TODO clear callback
-    }
-    
-    func clientSideBridgeRecivedMessage(message: XCTUIBridgeMessage) {
-        
-        delegate?.bridgeReceivedMessage(message)
-    }
-    
+
     func notificationRecieved(notification:NSNotification) {
-        if let userInfoDictionary = notification.userInfo,
-            identifier = userInfoDictionary["identifier"] as? String,
-            bridgeSide = XCTUIBridgeSide(rawValue: identifier),
-            payload = userInfoDictionary["payload"] {
-                do {
-                    let message = try XCTUIBridgeMessage(dictionary: payload as! [String:AnyObject] )
-                    switch bridgeSide {
-                    case .Test:
-                        testSideBridgeRecivedMessage(message)
-                        break
-                    case .Client:
-                        clientSideBridgeRecivedMessage(message)
-                        break
-                    }
-                } catch {
-                
+        if let payload = notification.userInfo,
+            identifier = payload["name"] as? String {
+                clientListeners[identifier]?.forEach {$0()}
+        }
+    }
+    static public func sendNotification(identifier: String) {
+        CFNotificationCenterPostNotification(CFNotificationCenterGetDarwinNotifyCenter(), identifier as CFString, nil, nil, true)
+    }
+    
+    
+    static public func register(identifier: String, completion: XCTUIBridgeCallback) -> XCTUIBridgeRemover {
+        if let _ = instance.clientListeners[identifier] {
+            instance.clientListeners[identifier]!.append(completion)
+            let remover:XCTUIBridgeRemover = {
+                if let index = instance.clientListeners[identifier]!.indexOf(completion) {
+                    instance.clientListeners[identifier]!.removeAtIndex(index)
                 }
+            }
+            return remover
+        } else {
+            instance.clientListeners[identifier] = [XCTUIBridgeCallback]()
+            return register(identifier, completion: completion)
         }
     }
     
-    static private func registerForDarwinNotificationsWithIdentifier(identifier: String) {
-        let center = CFNotificationCenterGetLocalCenter()
-        let suspensionBehavior = CFNotificationSuspensionBehavior.DeliverImmediately
-        
+    static private func registerForDarwinNotification(identifier: String) {
         let callback: @convention(block) (CFNotificationCenter!, UnsafeMutablePointer<Void>, CFString!, UnsafePointer<Void>, CFDictionary!) -> Void = { (center, observer, name, object, userInfo) in
-            if let name = name,
-                userInfo = userInfo {
-                    let userInfoDictionary = ["identifier" : name, "payload" : userInfo] as [NSObject: AnyObject]
-                    NSNotificationCenter.defaultCenter().postNotificationName(XCTUIBridgeNotification, object: nil, userInfo: userInfoDictionary)
-            }
+            NSNotificationCenter.defaultCenter().postNotificationName(XCTUIBridgeNotification, object: nil, userInfo: ["name":name])
         }
         
         let imp: COpaquePointer = imp_implementationWithBlock(unsafeBitCast(callback, AnyObject.self))
-        let notificationCallback: CFNotificationCallback = unsafeBitCast(imp, CFNotificationCallback.self)
-        CFNotificationCenterAddObserver(center, nil, notificationCallback, identifier, nil, suspensionBehavior)
+        let notificationCallback = unsafeBitCast(imp, CFNotificationCallback.self)
+        
+        CFNotificationCenterAddObserver(CFNotificationCenterGetDarwinNotifyCenter(), unsafeAddressOf(self), notificationCallback, identifier, nil, .DeliverImmediately)
     }
-    
-    static private func sendDarwinNotificationWithIdentifier(name: String, payload: CFDictionaryRef) {
-        let center = CFNotificationCenterGetLocalCenter()
-        CFNotificationCenterPostNotification(center, name as CFString, nil, payload, true)
-    }
+
 }
